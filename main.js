85ad00775fb750b1b190d2434ce7a75abefe851a
Ericcentrik <enaiman1@gmail.com>
2019-12-19 23:02:35 -0500

    bug fix, now able to tap into process object by adding nodeIntegration

--- main.js
+++ main.js
@@ -8,24 +8,32 @@ const url = require('url');
 */
 let win;
 
+
 // funtion to create window object
 createWindow = ()=> {
 // create browser windown
-win = new BrowserWindow({width:800, height:600, icon:__dirname+"/img/gear.png"})
+win = new BrowserWindow({
+    width:800, 
+    height:600,
+    webPreferences: {
+        nodeIntegration: true
+      } 
+})
 
 // loads index.html
 win.loadURL(url.format({
-    pathname: path.join(__dirname, "index.html"),
-    protocol: 'file',
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
     slashes: true
-}))
+}));
 
 // open devtools
-win.webContents().openDevTools()
+win.webContents.openDevTools()
 
 win.on('closed', () => {
     win = null;
 })
+
 }
 // run create wibdow function
 app.on('ready', createWindow);
@@ -34,5 +42,6 @@ app.on('ready', createWindow);
 app.on('window-all-closed', () => {
 if (process.platform !== 'darwin') {
     app.quit()
+    
 }
 });
